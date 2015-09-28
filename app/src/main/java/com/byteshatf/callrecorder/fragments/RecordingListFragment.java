e75bca2f5b1ce58046c0fd03535d158c94ac5e09
s9iper1 <s9iper1@gmail.com>
2015-09-28 18:56:49 +0500

    code clean

--- app/src/main/java/com/byteshatf/callrecorder/fragments/RecordingListFragment.java
+++ app/src/main/java/com/byteshatf/callrecorder/fragments/RecordingListFragment.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -23,8 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class RecordingListFragment extends android.support.v4.app.Fragment implements
-        AdapterView.OnItemClickListener {
+public class RecordingListFragment extends android.support.v4.app.Fragment {
 
     private ArrayAdapter<String> arrayAdapter;
     private View view;
@@ -45,37 +43,8 @@ public class RecordingListFragment extends android.support.v4.app.Fragment imple
         super.onResume();
         arrayList = mHelpers.getAllFilesFromFolder();
         arrayAdapter = new RecordingAdapter(getActivity(), android.R.layout.simple_list_item_1, arrayList);
-//        mRecordList.setOnItemLongClickListener(this);
-        mRecordList.setOnItemClickListener(this);
         mRecordList.setAdapter(arrayAdapter);
     }
-
-//    @Override
-//    public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        builder.setTitle("Delete");
-//        builder.setMessage("Do you want to delete this recording?");
-//        builder.setCancelable(false);
-//        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String path = AppGlobals.getDataDirectory("CallRec")+ "/" + parent.getItemAtPosition(position) ;
-//                removeFiles(path);
-//                String item = arrayAdapter.getItem(position);
-//                arrayAdapter.remove(item);
-//                arrayAdapter.notifyDataSetChanged();
-//            }
-//        });
-//        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.dismiss();
-//            }
-//        });
-//        builder.create();
-//        builder.show();
-//        return true;
-//    }
 
     private void removeFiles(String path) {
         File file = new File(path);
@@ -87,22 +56,6 @@ public class RecordingListFragment extends android.support.v4.app.Fragment imple
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String path = AppGlobals.getDataDirectory("CallRec") + "/" + parent.getItemAtPosition(position);
-        try {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_VIEW);
-            File file = new File(path);
-            intent.setPackage("com.google.android.music");
-            intent.setDataAndType(Uri.fromFile(file), "audio/*");
-            System.out.println(path);
-            startActivity(intent);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
     }
 
@@ -119,9 +72,10 @@ public class RecordingListFragment extends android.support.v4.app.Fragment imple
                 LayoutInflater inflater = getActivity().getLayoutInflater();
                 convertView = inflater.inflate(R.layout.recording_cutom_listview, parent, false);
                 holder = new ViewHolder();
-                holder.title = (TextView) convertView.findViewById(R.id.FilePath);
+                holder.title = (TextView) convertView.findViewById(R.id.recordingList);
                 holder.deleteButton = (Button) convertView.findViewById(R.id.delete_button);
                 holder.deleteButton.setOnClickListener(this);
+                holder.title.setOnClickListener(this);
                 convertView.setTag(holder);
             } else {
                 holder = (ViewHolder) convertView.getTag();
@@ -133,9 +87,9 @@ public class RecordingListFragment extends android.support.v4.app.Fragment imple
 
         @Override
         public void onClick(View v) {
+            final int position = mRecordList.getPositionForView((View) v.getParent());
             switch (v.getId()) {
                 case R.id.delete_button:
-                    final int position = mRecordList.getPositionForView((View) v.getParent());
                     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                     builder.setTitle("Delete");
                     builder.setIcon(android.R.drawable.ic_menu_delete);
@@ -159,6 +113,21 @@ public class RecordingListFragment extends android.support.v4.app.Fragment imple
                     });
                     builder.create();
                     builder.show();
+                    break;
+                case R.id.recordingList:
+                    String path = AppGlobals.getDataDirectory("CallRec") + "/" + mRecordList.getItemAtPosition(position);
+                    try {
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        File file = new File(path);
+                        intent.setPackage("com.google.android.music");
+                        intent.setDataAndType(Uri.fromFile(file), "audio/*");
+                        System.out.println(path);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+
                     break;
             }
         }
